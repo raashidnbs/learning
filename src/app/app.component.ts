f291820c041f119aec01fd903bb195e539a400e2
raashidnbs <rashid.ahmad@northbaysolutions.net>
2021-11-26 20:29:41 +0500

    Added autocomplete from material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable, of} from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'learning';
+  title = 'Learning App';
+  persons: Person[] = [{
+    "id": 1,
+    "name": "Rashid Ahmad"
+  }, {
+    "id": 2,
+    "name": "Waseem Akram"
+  }];
+
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions!: Observable<string[]>;
+  filteredPersons!: Observable<Person[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+
+    this.filteredPersons = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.filterPersons(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  private filterPersons(value: string) {
+    if (value) {
+      const filterValue = value.toLowerCase();
+      return this.persons.filter(person => person.name.toLowerCase().includes(filterValue));
+    } else {
+      return this.persons;
+    }
+    
+  }
+
+  displayFn(person: Person): string {
+    return person && person.name ? person.name : '';
+  }
 }
+
+export default interface Person {
+  id: number;
+  name: string;
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const materialModules = [
+  ReactiveFormsModule,
+  MatAutocompleteModule,
+  MatSliderModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    materialModules
+  ],
+  exports: [
+    materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
